21f50d58f12fa0c681aaf5bb2e7748f81517a481
akash <akasheldo@hotmail.com>
2020-12-09 20:51:13 -0500

    Connected to prismic

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,60 +9,68 @@ import {
   ScrollAppBar,
   StyleSheet,
 } from '@components';
+import {
+  Client,
+  Graphic,
+  HomePageSection,
+  Review,
+  Slice,
+} from 'prismic-configuration';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
 
-const IndexPage = () => {
+interface HomePageData {
+  hero_title: RichTextBlock[];
+  hero_description: RichTextBlock[];
+  hero_image: Graphic;
+  body: Slice[];
+}
+
+interface Props {
+  hero: {
+    title: RichTextBlock[];
+    description: RichTextBlock[];
+    image: Graphic;
+  };
+  sections: HomePageSection[];
+  reviews: Review[];
+}
+
+export async function getStaticProps() {
+  const home: HomePageData = (await Client().getSingle('homepage', {})).data;
+
+  return {
+    props: {
+      hero: {
+        title: home.hero_title,
+        description: home.hero_description,
+        image: home.hero_image,
+      },
+      sections: home.body[0].items as HomePageSection[],
+      reviews: home.body[1].items as Review[],
+    },
+  };
+}
+
+const IndexPage: React.FC<Props> = ({ hero, sections, reviews }) => {
   return (
     <div style={{ overflowX: 'hidden', margin: -8 }}>
       <ScrollAppBar />
-      <HeroSection />
-      <ContentContainer alt>
-        <Grid item xs={12} md={6} style={styles.gridColumn}>
-          <Header>Conjugation Info</Header>
-          <Box mt={5} pb={16}>
-            <Typography variant="body1" color="textSecondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image src="/screenshot.png" width={400} height={800} />
-        </Grid>
-      </ContentContainer>
-      <ContentContainer>
-        <Grid item xs={12} md={6} style={styles.gridColumn}>
-          <Header>Conjugation Info</Header>
-          <Box mt={5} pb={16}>
-            <Typography variant="body1" color="textSecondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image src="/screenshot.png" width={400} height={800} />
-        </Grid>
-      </ContentContainer>
-      <ContentContainer alt>
-        <Grid item xs={12} md={6} style={styles.gridColumn}>
-          <Header>Conjugation Info</Header>
-          <Box mt={5} pb={16}>
-            <Typography variant="body1" color="textSecondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image src="/screenshot.png" width={400} height={800} />
-        </Grid>
-      </ContentContainer>
+      <HeroSection {...hero} />
+      {sections.map((section, index) => (
+        <ContentContainer alt={index % 2 === 0}>
+          <Grid item xs={12} md={6} style={styles.gridColumn}>
+            <Header>{RichText.asText(section.header)}</Header>
+            <Box mt={5} pb={16}>
+              <Typography variant="body1" color="textSecondary">
+                {RichText.asText(section.description)}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Image src={section.graphic.url} width={400} height={800} />
+          </Grid>
+        </ContentContainer>
+      ))}
       <ReviewSection />
     </div>
   );
