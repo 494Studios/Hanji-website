36b8b18d8e9d86f6cae466eb853b145a5343e4ce
akash <akasheldo@hotmail.com>
2020-12-11 22:12:12 -0500

    Connected to mailerlite

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,7 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import MailerLite from 'mailerlite-api-v2-node';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ name: 'John Doe' }));
+const GROUP_ID = 105294742;
+const mailerLite = MailerLite('0f4c8ec7ca634dd1e112865da2f0517a');
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const name = req.body.name;
+  const email = req.body.email;
+
+  if (!name || !email) {
+    res.statusCode = 400;
+    res.end('ERROR: name and/or email missing');
+    return;
+  }
+
+  const response = await mailerLite.addSubscriberToGroup(GROUP_ID, {
+    name,
+    email,
+  });
+
+  // @ts-ignore
+  if (response.email !== email) {
+    res.statusCode = 500;
+    res.end(`Error occured when adding subscriber`);
+  } else {
+    res.statusCode = 200;
+    res.end();
+  }
 }
