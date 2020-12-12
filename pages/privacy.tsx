b6e654426b11d28d57b76933a875b7b25a567248
akash <akasheldo@hotmail.com>
2020-12-12 15:01:25 -0500

    Created privacy policy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,46 @@
+import { FooterSection, ScrollAppBar } from '@components';
+import {
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@material-ui/core';
+import { Client } from 'prismic-configuration';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
+import React, { FC } from 'react';
+import htmlSerializer from 'utils/htmlSerializer';
+
+interface PrivacyPageData {
+  title: RichTextBlock[];
+  content: RichTextBlock[];
+}
+
+export async function getStaticProps() {
+  const home: PrivacyPageData = (await Client().getSingle('privacy_policy', {}))
+    .data;
+
+  return { props: home };
+}
+
+const PrivacyPage: FC<PrivacyPageData> = ({ title, content }) => {
+  const { palette } = useTheme();
+
+  return (
+    <div style={{ overflowX: 'hidden', margin: -8 }}>
+      <ScrollAppBar />
+      <Toolbar style={{ background: palette.primary.main }} />
+      <Container maxWidth="md" style={{ paddingBottom: 32 }}>
+        <Box mt={3} mb={8}>
+          <Typography variant="h3" align="center">
+            {RichText.asText(title)}
+          </Typography>
+        </Box>
+        <RichText render={content} htmlSerializer={htmlSerializer} />
+      </Container>
+      <FooterSection />
+    </div>
+  );
+};
+
+export default PrivacyPage;
