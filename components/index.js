7aef3058e14b33138245d767523e84e2a677edaa
akash <akasheldo@hotmail.com>
2020-12-19 14:56:26 -0500

    Finished refactoring

--- components/index.js
+++ components/index.js
@@ -2,16 +2,16 @@ export { default as ContentContainer } from './ContentContainer';
 export { default as FooterSection } from './FooterSection';
 export { default as Header } from './Header';
 export { default as HeroSection } from './HeroSection';
-export { default as ReviewCard } from './ReviewCard';
-export { default as ReviewSection } from './ReviewSection';
 export { default as ScrollAppBar } from './ScrollAppBar';
 export { default as StyleSheet } from './StyleSheet';
-export { default as WaitListForm } from './WaitListForm';
-export { default as WaitListSection } from './WaitListSection';
 
 // Main page
 export { default as MainHeroContent } from './main/HeroContent';
 export { default as MainHeroSection } from './main/HeroSection';
+export { default as ReviewCard } from './main/ReviewCard';
+export { default as ReviewSection } from './main/ReviewSection';
+export { default as WaitListForm } from './main/WaitListForm';
+export { default as WaitListSection } from './main/WaitListSection';
 
 // About page
 export { default as AboutHeroContent } from './about/HeroContent';
