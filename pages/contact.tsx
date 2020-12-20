e89cd6b02a89e57dab672938efb819904f43f85b
akash <akasheldo@hotmail.com>
2020-12-19 21:19:57 -0500

    Removed unneed code

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import React, { FC, useEffect } from 'react';
 
 const ContactPage: FC = () => {
@@ -28,17 +28,6 @@ const ContactPage: FC = () => {
         className="ml-subscribe-form ml-subscribe-form-3183544"
       >
         <div className="ml-vertical-align-center">
-          {/* <div
-            className="subscribe-form ml-block-success"
-            style={{ display: 'none' }}
-          >
-            <div className="form-section">
-              <p>
-                Thank you, you have successfully subscribed to our announcement
-                list!
-              </p>
-            </div>
-          </div> */}
           <form
             className="ml-block-form"
             action="https://landing.mailerlite.com/webforms/submit/o0s0f4"
@@ -47,10 +36,6 @@ const ContactPage: FC = () => {
             method="POST"
             target="_blank"
           >
-            <p className="signup-title">
-              Sign up for our announcement list and we'll let you know when we
-              launch!
-            </p>
             <div className="subscribe-form">
               <div className="form-section">
                 <div className="form-group ml-field-email ml-validate-required ml-validate-email">
@@ -64,35 +49,10 @@ const ContactPage: FC = () => {
                     autoCapitalize="off"
                     autoCorrect="off"
                   />
-                  {/* <input
-                    type="email"
-                    name="fields[email]"
-                    className="form-control signup-text"
-                    placeholder="Email address"
-                    autoComplete="email"
-                    spellCheck="false"
-                    autoCapitalize="off"
-                    autoCorrect="off"
-                  /> */}
                   <input type="hidden" name="ml-submit" value="1" />
-                  <button type="submit" className="primary signup-button">
-                    Subscribe
-                  </button>
-                  <button
-                    disabled
-                    style={{ display: 'none' }}
-                    type="button"
-                    className="loading"
-                  >
-                    Submitting...
-                    {/* <img
-                  src="https://static.mailerlite.com/images/rolling@2x.gif"
-                  alt="loading..."
-                  width="20"
-                  height="20"
-                  style={{ width: '20px', height: '20px' }}
-                /> */}
-                  </button>
+                  <Button type="submit" className="primary signup-button">
+                    Submit
+                  </Button>
                 </div>
               </div>
             </div>
